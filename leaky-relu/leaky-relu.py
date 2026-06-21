9e99f5f3e5a4970ecac808f39b1a8483ea4913eb
Trần Minh Khôi <tranminhkhoi8407@gmail.com>
2026-06-21 14:51:55 +0700

    Solve leaky-relu

--- leaky-relu/leaky-relu.py
+++ leaky-relu/leaky-relu.py
@@ -3,10 +3,12 @@ import numpy as np
 def leaky_relu(x, alpha=0.01):
     x = np.array(x, dtype=float)
 
-    l = len(x)
+    # l = len(x)
 
-    for i in range(l):
-        if x[i] < 0:
-            x[i] = alpha * x[i]
+    # for i in range(l):
+    #     if x[i] < 0:
+    #         x[i] = alpha * x[i]
 
-    return x
+    # return x
+
+    return np.maximum(alpha * x, x)
