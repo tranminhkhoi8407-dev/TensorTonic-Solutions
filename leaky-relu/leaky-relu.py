db3d802289acb53a7777186eb7172baf3e2a63e3
Trần Minh Khôi <tranminhkhoi8407@gmail.com>
2026-06-21 14:51:06 +0700

    Solve leaky-relu

---
+++ leaky-relu/leaky-relu.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+def leaky_relu(x, alpha=0.01):
+    x = np.array(x, dtype=float)
+
+    l = len(x)
+
+    for i in range(l):
+        if x[i] < 0:
+            x[i] = alpha * x[i]
+
+    return x
